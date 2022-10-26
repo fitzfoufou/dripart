052ffd06e0608e50ad85a01d844ef20f8d46c8be
Fitzfoufou <francois.fitzpatrick@gmail.com>
2022-10-26 09:11:30 -0400

    lint(routes_rspec.rb): fix quotes

--- spec/routes_spec.rb
+++ spec/routes_spec.rb
@@ -1,7 +1,7 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "routes", :type => :routing do
+RSpec.describe 'routes', type: :routing do
   it "routes root to pages controller's main method" do
-    expect(get("/")).to route_to("pages#main")
+    expect(get('/')).to route_to('pages#main')
   end
 end
